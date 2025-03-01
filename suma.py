f3587c56a21f264e729cf6b40e9569adfdd880ae
odesarrollo25 <daniimoz010@gmail.com>
2025-03-01 16:36:22 -0500

    suma

---
+++ suma.py
@@ -0,0 +1,9 @@
+''' 
+Para sumar dos numeros en Python vamos a crear 3 variables
+num1, num2 y result, posteriormente vamos a imprimir
+'''
+num1 = 5
+num2 = 3
+result = num1+num2
+
+print(f'El resultado de la suma ', num1, "+", num2, "=", result)
