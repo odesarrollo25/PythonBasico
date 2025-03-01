dd6cb35ea3f49eb5e174db4f7bcdf198c0952481
odesarrollo25 <daniimoz010@gmail.com>
2025-03-01 17:57:06 -0500

    po

---
+++ operadores Logicos/not.py
@@ -0,0 +1,9 @@
+''' 
+Para representar los operadores logicos Python vamos a crear 3 variables
+num1, num2 y result, posteriormente vamos a imprimir
+'''
+num1 = not True
+num2 = False
+result = num1 and num2
+
+print(f'El resultado', num1, "and", num2, "=", result)
